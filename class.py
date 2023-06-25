2b6c647aedfeae3fdcd631ca49ed8df2f8c67026
ycu2001 <130482087+ycu2001@users.noreply.github.com>
2023-06-25 22:31:36 +0800

    Add files via upload

---
+++ class.py
@@ -0,0 +1,14 @@
+class Dog():
+    def """__請填這裡__"""(self, type, bloodType, age, favoriteFood):
+        """start your code here"""
+        
+        """end your code"""
+    def bark("""__請填這裡__"""):
+        print("汪汪汪")
+
+buddy = Dog("黃金獵犬", "A", 7, "肉")
+print("他是一隻"+"""__請填這裡__""")
+print("他喜歡吃"+"""__請填這裡__""")
+print("血型："+"""__請填這裡__""")
+print("年齡：","""__請填這裡__""")
+"""__請填這裡__""" #汪汪汪
---
+++ if_bmi.py
@@ -0,0 +1,14 @@
+w = int(input("請輸入您的體重:"))
+h = int(input("請輸入您的身高:"))
+
+bmi = "___fill in___" #BMI算法
+
+print(bmi)
+print("您的BMI為 %.2f" %(bmi)) #%.2f 取到小數到二位
+
+if "___fill in___":
+  print("體重過輕,會被風吹走")
+elif "___fill in___":
+  print("正常範圍")
+else:
+  print("過重喔!!")
