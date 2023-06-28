0edc0a7a2b73f5f87068d75bdc0c68471c6490c6
ycu2001 <130482087+ycu2001@users.noreply.github.com>
2023-06-28 10:04:21 +0800

    Update 2_if_bmi.py

--- 2_if_bmi.py
+++ 2_if_bmi.py
@@ -1,14 +1,14 @@
 w = int(input("請輸入您的體重:"))
 h = float(input("請輸入您的身高(公尺):"))
 
-bmi = ___fill_in___ #BMI算法（身高單位是公尺哦）
+bmi = "___fill_in___" #BMI算法（身高單位是公尺哦）
 
 print(bmi)
 print("您的BMI為 %.2f" %(bmi)) #%.2f 取到小數到二位
 
-if __fill_in__:
+if "__fill_in__":
   print("體重過輕,會被風吹走")
-elif __fill_in__:
+elif "__fill_in__":
   print("正常範圍")
 else:
   print("過重喔!!")
