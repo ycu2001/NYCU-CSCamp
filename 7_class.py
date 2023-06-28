459b0dc5b9e39eabb7c0660c8bc994a750e24603
ycu2001 <130482087+ycu2001@users.noreply.github.com>
2023-06-28 10:05:12 +0800

    Update 7_class.py

--- 7_class.py
+++ 7_class.py
@@ -1,14 +1,14 @@
 class Dog():
-    def __fill_in__(self, type, bloodType, age, favoriteFood):
+    def "__fill_in__"(self, type, bloodType, age, favoriteFood):
         """start your code here"""
         
         """end your code"""
-    def bark("""__請填這裡__"""):
+    def bark("__fill_in__"):
         print("汪汪汪")
 
 buddy = Dog("黃金獵犬", "A", 7, "肉")
-print("他是一隻"+__fill_in__)
-print("他喜歡吃"+__fill_in__)
-print("血型："+__fill_in__)
-print("年齡：",__fill_in__)
-__fill_in__ #汪汪汪
+print("他是一隻"+"__fill_in__")
+print("他喜歡吃"+"__fill_in__")
+print("血型："+"__fill_in__")
+print("年齡：","__fill_in__")
+"__fill_in__" #汪汪汪
